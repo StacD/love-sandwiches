7d95df7d69e3d873c3284d28ca54cd04f811a8ec
StacD <>
2023-11-10 14:56:59 +0000

    re pasted code due to workspace crash

--- run.py
+++ run.py
@@ -69,7 +69,6 @@ sales_worksheet = SHEET.worksheet("sales")
 sales_worksheet.append_row(data)
 print("Sales worksheet updated successfully.\n")
 
-
 def update_surplus_worksheet(data):
 
 -Update surplus worksheet, add new row with the list data provided.
@@ -128,6 +127,22 @@ def get_last_5_entries_sales():
     return columns
 
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 def main():
     """
     Run all program functions
@@ -137,9 +152,10 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    print(stock_data)
 
 
 print("Welcome to Love Sandwiches Data Automation")
-# main()
-
-sales_columns = get_last_5_entries_sales()
+main()
